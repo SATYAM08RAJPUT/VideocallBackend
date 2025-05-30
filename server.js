860988532a17affd2adedc745b93a94eb9756fdd
Satyam Rajput <satyamrajput081998@gmail.com>
2025-05-30 06:27:21 -0400

    add multiple users

--- server.js
+++ server.js
@@ -46,6 +46,54 @@
 //   console.log(`Signaling server running on port ${PORT}`)
 // );
 
+// import express from "express";
+// import http from "http";
+// import { Server } from "socket.io";
+// import cors from "cors";
+
+// const app = express();
+// app.use(cors());
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "https://benevolent-llama-48fa6d.netlify.app",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// const rooms = {};
+// io.on("connection", (socket) => {
+//   socket.on("join-room", (roomId) => {
+//     const room = io.sockets.adapter.rooms.get(roomId);
+//     const numClients = room ? room.size : 0;
+
+//     socket.join(roomId);
+
+//     if (numClients === 0) {
+//       socket.emit("created");
+//     } else if (numClients === 1) {
+//       socket.emit("joined");
+//       socket.to(roomId).emit("ready");
+//     } else {
+//       socket.emit("full");
+//     }
+
+//     socket.on("signal", ({ data }) => {
+//       socket.to(roomId).emit("signal", { data });
+//     });
+
+//     socket.on("disconnect", () => {
+//       socket.to(roomId).emit("user-left");
+//     });
+//   });
+// });
+
+// const PORT = 7007;
+// server.listen(PORT, () =>
+//   console.log(`Signaling server running on port ${PORT}`)
+// );
+
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
@@ -62,34 +110,26 @@ const io = new Server(server, {
   },
 });
 
-const rooms = {};
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
-    const room = io.sockets.adapter.rooms.get(roomId);
-    const numClients = room ? room.size : 0;
-
+  socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
 
-    if (numClients === 0) {
-      socket.emit("created");
-    } else if (numClients === 1) {
-      socket.emit("joined");
-      socket.to(roomId).emit("ready");
-    } else {
-      socket.emit("full");
-    }
+    // Notify others in the room that a new user joined
+    socket.to(roomId).emit("user-joined", userId);
 
-    socket.on("signal", ({ data }) => {
-      socket.to(roomId).emit("signal", { data });
+    // Relay signal data between users
+    socket.on("signal", ({ to, from, data }) => {
+      io.to(to).emit("signal", { from, data });
     });
 
+    // Notify others when a user leaves
     socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-left");
+      socket.to(roomId).emit("user-left", userId);
     });
   });
 });
 
 const PORT = 7007;
-server.listen(PORT, () =>
-  console.log(`Signaling server running on port ${PORT}`)
-);
+server.listen(PORT, () => {
+  console.log(`Signaling server running on port ${PORT}`);
+});
